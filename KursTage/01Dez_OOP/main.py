397d2f7d90e71ca061c19f44068e50c462d00fd6
Oguzhan-Karaca <karacaoguzhan@outlook.de>
2025-12-05 11:03:07 +0100

    Füge buecher.txt mit Buchdaten für die Import-Funktionalität hinzu

--- KursTage/01Dez_OOP/main.py
+++ KursTage/01Dez_OOP/main.py
@@ -2,16 +2,25 @@ from buch import Buch
 from dvd import Dvd
 from bibliothek import Bibliothek
 
+def importiere_buecher(bibliothek, datei="buecher.txt"):
+    with open(datei, encoding="utf-8") as file:
+        inhalt = file.read()
+
+    for zeile in inhalt.splitlines():
+        teile = zeile.split(",")
+        buecher = buecher(teile[0], int(teile[1]), teile[2], int(teile[3]))
+        self.hinzufuegen(buecher)
+
 # Bibliothek erstellen
 bibo = Bibliothek()
 
-# Medien erstellen
-buch1 = Buch("Harry Potter", 1997, "J.K. Rowling", 336)
-dvd1 = Dvd("Inception", 2010, 148, "Christopher Nolan")
+# # Medien erstellen
+# buch1 = Buch("Harry Potter", 1997, "J.K. Rowling", 336)
+# dvd1 = Dvd("Inception", 2010, 148, "Christopher Nolan")
 
-# Zur Bibliothek hinzufuegen
-bibo.hinzufuegen(buch1)
-bibo.hinzufuegen(dvd1)
+# # Zur Bibliothek hinzufuegen
+# bibo.hinzufuegen(buch1)
+# bibo.hinzufuegen(dvd1)
 
 # Auflisten
 bibo.auflisten()
--- KursTage/02Dez_OOP/aufgabe.md
+++ KursTage/02Dez_OOP/aufgabe.md
@@ -8,18 +8,21 @@ Harry Potter and the Philosopher's Stone,1997,J.K. Rowling,223
 ```
 
 Einlesen einer (utf-8-kodierten) Datei in einen String:
+
 ```python
 with open("datei.txt", encoding="utf-8") as file:
     komplettstring = file.read()
 ```
 
 Einen String anhand eines Trenners splitten:
+
 ```python
 s = "abc-def-012"
 liste = s.split("-") # ['abc', 'def', '012']
 ```
 
 Einen String der mehrere Zeilen enthält in einzelne Zeilen splitten:
+
 ```python
 s = "abc\ndef\n"
 zeilen = s.splitlines() # ['abc', 'def']
